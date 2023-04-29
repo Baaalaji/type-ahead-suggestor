5e1490a247053af96633d23328652b26445658c6
b0a04gl <balaji.arumugam@walmart.com>
2023-04-29 13:19:44 +0530

    init commit

---
+++ src/main/java/com/personal/type/ahead/suggestor/TypeAheadSuggestorApplication.java
@@ -0,0 +1,13 @@
+package com.personal.type.ahead.suggestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeAheadSuggestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeAheadSuggestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/type/ahead/suggestor/TypeAheadSuggestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.type.ahead.suggestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeAheadSuggestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
